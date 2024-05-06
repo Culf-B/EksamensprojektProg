ee5398de0654f195428adbebf1f7e01bcbbc094b
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-05-06 15:30:55 +0200

    Some work on simulation class

--- classes/simulation.js
+++ classes/simulation.js
@@ -1,7 +1,42 @@
-class Simulation extends Window {
-    constructor() {
+class Simulation extends Window
+{
+    constructor(v0, g, a)
+    {
+        super(width / 2, 0, width / 2, height / 2, {}, [0, 0, 0]);
+
+        // Var setup
+        this.t = 0;
+        this.v0 = v0;
+        this.g = g;
+        this.a = a;
+
+        // Calculate end x position
+        this.xEnd = ((pow(this.v0, 2) * sin(2 * this.a)) / this.g);
+
+        // Calculate max height
+        this.yMax = pow((this.v0 * sin(this.a)), 2) / (2 * this.g);
+
+        // Create ball
+        this.ball = new Ball(this.v0, this.g, this.a);
+    }
+
+    update()
+    {
         
     }
 
-    
+    start()
+    {
+
+    }
+
+    pause()
+    {
+
+    }
+
+    step()
+    {
+
+    }
 }
