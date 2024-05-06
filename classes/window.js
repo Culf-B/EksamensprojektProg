0af52a2ac1430e0769197f7a707470ff7a72e9f2
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-05-06 14:50:38 +0200

    Window class

--- classes/window.js
+++ classes/window.js
@@ -1,5 +1,43 @@
-class Window {
-    constructor() {
-        
+class Window
+{
+    constructor(x, y, width, height, graphics, bgColor)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.surface = createGraphics(width, height);
+        this.graphics = graphics;
+        this.bgColor = bgColor
+    }
+
+    updateGraphics()
+    {
+        this.graphics.forEach(obj => {
+            if (obj.type == "classObject")
+            {
+                obj.object.draw(this.surface);
+            }
+            else
+            {
+                this.surface.fill(obj.color[0], obj.color[1], obj.color[2]);
+                this.surface.stroke(obj.color[0], obj.color[1], obj.color[2]);
+                if (obj.type == "text")
+                {
+                    this.surface.text(obj.text, obj.x, obj.y);
+                }
+                else if (obj.type == "line")
+                {
+                    this.surface.line(obj.x1, obj.y1, obj.x2, obj.y2);
+                }
+            }
+        });
+    }
+
+    drawWindow()
+    {
+        this.surface.background(this.bgColor);
+        this.updateGraphics()
+        image(this.surface, this.x, this.y);
     }
 }
