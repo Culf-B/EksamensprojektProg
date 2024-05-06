8642230504d1172c93676401cf73f25eee5341de
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-05-06 15:23:13 +0200

    Ball class

--- classes/ball.js
+++ classes/ball.js
@@ -1,5 +1,30 @@
 class Ball {
-    constructor() {
+    constructor(v0, g, a)
+    {
+        // Vars setup
+        this.v0 = v0;
+        this.g = g;
+        this.a = a;
+        this.v0x = this.v0 * cos(this.a);
+        this.v0y = this.v0 * sin(this.a);
+
+        this.x = 0;
+        this.y = 0;
         
+        this.r = 10;
+    }
+
+    updatePos(t)
+    {
+        this.x = this.v0x * t;
+        this.y = 0.5 * (-this.g) * pow(t, 2) + this.v0y * t;
+    }
+
+    draw(surface)
+    {
+        surface.stroke(0);
+        surface.fill(255);
+
+        surface.circle(tihs.x, this.y, this.r * 2);
     }
 }
