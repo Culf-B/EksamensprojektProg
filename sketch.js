4bc705b1a0f61333e4fb394e7f7193d3a70f9571
ArnoldJ22R <amj@mannen.dk>
2024-05-06 13:39:28 +0200

    skibidi

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
   createCanvas(400, 400);
+  //sliders for values
+  tyndeacceleration = createSlider(0, 90 , 45, 1);
+  tyndeacceleration.position();
+  tyndeacceleration.size();
+
 }
 
 function draw() {
   background(220);
+  g = tyndeacceleration.value();
 }
